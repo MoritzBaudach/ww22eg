a40f2864ab54b0309fe0a460d65973f721896c8c
Moritz <Moritz.Krischke@Vodafone.com>
2023-10-12 15:21:31 +0200

    Get, DELTE, java persists

--- src/main/java/de/dhbw/wwieeg22spring/controller/UserController.java
+++ src/main/java/de/dhbw/wwieeg22spring/controller/UserController.java
@@ -3,10 +3,9 @@ package de.dhbw.wwieeg22spring.controller;
 import de.dhbw.wwieeg22spring.controller.requests.UserRequest;
 import de.dhbw.wwieeg22spring.controller.responses.User;
 import de.dhbw.wwieeg22spring.services.UserService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -26,6 +25,24 @@ public class UserController {
         );
         return user;
     }
+
+    @GetMapping("/users/{userid}")
+    public User getUser(@PathVariable("userid") Long id){
+        return userService.getUserById(id);
+    }
+
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userService.getAllUser();
+    }
+
+
+    @DeleteMapping("/users")
+    public void deleteUser(@RequestParam(value = "id", required = true) Long id){
+        userService.deleteUser(id);
+    }
+
 
 
 
--- src/main/java/de/dhbw/wwieeg22spring/services/UserService.java
+++ src/main/java/de/dhbw/wwieeg22spring/services/UserService.java
@@ -4,7 +4,9 @@ import de.dhbw.wwieeg22spring.controller.responses.User;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class UserService {
@@ -18,6 +20,19 @@ public class UserService {
     }
 
 
+    public User getUserById(Long id) {
+        return usersById.get(id);
+    }
 
+    public List<User> getAllUser() {
+        List<User> collect = usersById.entrySet()
+                .stream()
+                .map((x) -> x.getValue())
+                .collect(Collectors.toList());
+        return collect;
+    }
 
+    public void deleteUser(Long id) {
+        usersById.remove(id);
+    }
 }
